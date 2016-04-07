dbc4a12d9608eb4e4fabd23254c42470bffa2dd2
shawncrawley <shawncrawley@gmail.com>
2016-04-07 14:07:03 -0600

    Update main.js

--- tethysapp/mttop_mining/public/js/main.js
+++ tethysapp/mttop_mining/public/js/main.js
@@ -55,8 +55,9 @@ require(["dojo/dom",
                 LoadingPicture.addClass("hidden");
                 var MineVolume = data.value.features[0].attributes.SUM;
                 var MineArea = data.value.features[0].attributes.AREA;
+                var actualVolume = (MineVolume - (MineArea * elevation));
                 //The mine volume and area are given the variables MineVolume and MineArea, respectively, which will be used later to print the results to the screen.
-                $("#VolumeValue").text("Volume above elevation (cubic meters): " + (MineVolume - (MineArea * elevation));
+                $("#VolumeValue").text("Volume above elevation (cubic meters): " + actualVolume);
                 $("#AreaValue").text("Area within mine selection above elevation (square meters): " + MineArea);
             }, null);
         }
